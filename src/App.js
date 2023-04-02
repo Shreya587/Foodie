32265ffe643d084f77f29d99a55e0a835a371733
Shreya <shreya.kri25062002@gmail.com>
2023-04-03 01:11:14 +0530

    Added more changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Home from './screens/Home';
 import Login from './screens/Login';
+import Signup from './screens/Signup';
 import {
   BrowserRouter as Router,
   Routes,
@@ -16,6 +17,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home/>} />
         <Route exact path="/login" element={<Login/>} />
+        <Route exact path="/Createuser" element={<Signup/>} />
       </Routes>
 
     </div>
